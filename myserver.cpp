33139a2b4f2c4a403acf08b914c4a2ca52b7fd8f
Michał Jezierski <mj394327@students.mimuw.edu.pl>
2019-05-17 20:41:32 +0200

    init commit

---
+++ myserver.cpp
@@ -0,0 +1,47 @@
+// myserver.cpp
+
+#include "myserver.h"
+#include "mythread.h"
+
+std::vector<QByteArray> players;
+
+MyServer::MyServer(QObject *parent) :
+    QTcpServer(parent)
+{
+}
+
+void MyServer::startServer()
+{
+    int port = 1238;
+    players.resize(2);
+    players[0] = QByteArray();
+    players[1] = QByteArray();
+
+    if(!this->listen(QHostAddress::Any, port))
+    {
+        qDebug() << "Could not start server";
+    }
+    else
+    {
+        qDebug() << "Listening to port " << port << "...";
+    }
+}
+
+// This function is called by QTcpServer when a new connection is available.
+void MyServer::incomingConnection(qintptr socketDescriptor)
+{
+    static int player_number = 0;
+
+    // We have a new connection
+    qDebug() << socketDescriptor << " Connecting...";
+
+    // Every new connection will be run in a newly created thread
+    MyThread *thread = new MyThread(socketDescriptor, this, player_number);
+    player_number++;
+
+    // connect signal/slot
+    // once a thread is not needed, it will be beleted later
+    connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+
+    thread->start();
+}
---
+++ mythread.h
@@ -0,0 +1,42 @@
+#ifndef MYTHREAD_H
+#define MYTHREAD_H
+
+#include <QThread>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QDataStream>
+
+class Test {
+public:
+    Test(int number1, int number2): number1(number1), number2(number2) {
+        v.push_back(number1);
+        v.push_back(number2);
+    }
+
+    int number1;
+    int number2;
+    std::vector<int> v;
+};
+
+class MyThread : public QThread
+{
+    Q_OBJECT
+public:
+    explicit MyThread(qintptr ID, QObject *parent = 0, int number = -1);
+
+    void run();
+
+signals:
+    void error(QTcpSocket::SocketError socketerror);
+
+public slots:
+    void readyRead();
+    void disconnected();
+
+private:
+    QTcpSocket *socket;
+    qintptr socketDescriptor;
+    int number;
+};
+
+#endif // MYTHREAD_H
